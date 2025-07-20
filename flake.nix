9ffa901385a72574e734fb6c092986c952d20563
Geordie Powers <geordie.powers@gmail.com>
2025-07-20 19:53:13 -0400

    switch default color scheme

--- flake.nix
+++ flake.nix
@@ -167,7 +167,7 @@
               "onedark" = onedark-nvim;
               "catppuccin" = catppuccin-nvim;
               "catppuccin-mocha" = catppuccin-nvim;
-              "tokyonight" = tokyonight-nvim;
+              "tokyonight-night" = tokyonight-nvim;
               "tokyonight-day" = tokyonight-nvim;
             }
           );
@@ -379,7 +379,7 @@
           # you could also pass something else:
           # see :help nixCats
           themer = true;
-          colorscheme = "onedark";
+          colorscheme = "tokyonight-night";
         };
         extra = {
           # to keep the categories table from being filled with non category things that you want to pass
