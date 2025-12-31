14708fc50c2cb4e18ea732809eea8e50855239d6
Geordie Powers <geordie.powers@gmail.com>
2025-12-31 17:47:59 -0500

    fix todo comments not loading

--- lua/myLuaConf/plugins/todo-comments.lua
+++ lua/myLuaConf/plugins/todo-comments.lua
@@ -48,60 +48,62 @@ return {
   {
     "todo-comments.nvim",
     for_cat = 'general.always',
-    cmd = { "TodoTelescope" },
+    -- cmd = { "TodoTelescope" },
+    after = function(plugin)
+      require('todo-comments').setup({
+        keywords = {
+          FIX = {
+            icon = " ",      -- icon used for the sign, and in search results
+            color = "error",  -- can be a hex color, or a named color (see below)
+            -- signs = false, -- configure signs for some keywords individually
+            alt = {           -- a set of other keywords that all map to this FIX keywords
+              "FIXME", "BUG", "BUGFIX", "ISSUE", "DANGER"
+            },
+          },
+
+          ERROR = {
+            icon = " ",      -- icon used for the sign, and in search results
+            color = "error",  -- can be a hex color, or a named color (see below)
+            -- signs = false, -- configure signs for some keywords individually
+            alt = {           -- a set of other keywords that all map to this FIX keywords
+              "ERR",
+            },
+          },
+
+          TODO = { icon = " ", color = "error", alt = { "LATER" } },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = "󱦺 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = "󰍨 ", color = "hint" },
+          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+
+          -- MARK: CUSTOM:
+          THEME = { icon = " ", color = "info" },
+          DESC = { icon = "󰍨 ", color = "info", alt = { "INFO" } },
+          ASSERT = { icon = "⏲ ", color = "test", alt = { "ASSERTION", "CHECK", "GUARD" } },
+          LINK = { icon = " ", color = "link" },
+          GP = { icon = " ", color = "link" },
+          UTIL = { icon = " ", color = "warning" },
+          MARK = { icon = " ", color = "warning", alt = { "SECTION", "END" } },
+          HISTORY = { icon = "󱦺 " },
+        },
+        colors = {
+          error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+          warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+          info = { "DiagnosticInfo", "#2563EB" },
+          hint = { "DiagnosticHint", "#10B981" },
+          default = { "Identifier", "#7C3AED" },
+          test = { "Identifier", "#FF00FF" },
+          link = { "#2563EB" },
+        },
+      })
+    end,
     -- ft = "",
     -- keys = "",
     -- colorscheme = "",
     -- after = function (plugin)
     --
     -- end,
-    opts = {
-      keywords = {
-        FIX = {
-          icon = " ",      -- icon used for the sign, and in search results
-          color = "error",  -- can be a hex color, or a named color (see below)
-          -- signs = false, -- configure signs for some keywords individually
-          alt = {           -- a set of other keywords that all map to this FIX keywords
-            "FIXME", "BUG", "BUGFIX", "ISSUE", "DANGER"
-          },
-        },
-
-        ERROR = {
-          icon = " ",      -- icon used for the sign, and in search results
-          color = "error",  -- can be a hex color, or a named color (see below)
-          -- signs = false, -- configure signs for some keywords individually
-          alt = {           -- a set of other keywords that all map to this FIX keywords
-            "ERR",
-          },
-        },
-
-        TODO = { icon = " ", color = "error", alt = { "LATER" } },
-        HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-        PERF = { icon = "󱦺 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = "󰍨 ", color = "hint" },
-        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-
-        -- MARK: CUSTOM:
-        THEME = { icon = " ", color = "info" },
-        DESC = { icon = "󰍨 ", color = "info", alt = { "INFO" } },
-        ASSERT = { icon = "⏲ ", color = "test", alt = { "ASSERTION", "CHECK", "GUARD" } },
-        LINK = { icon = " ", color = "link" },
-        GP = { icon = " ", color = "link" },
-        UTIL = { icon = " ", color = "warning" },
-        MARK = { icon = " ", color = "warning", alt = { "SECTION", "END" } },
-        HISTORY = { icon = "󱦺 " },
-      },
-      colors = {
-        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-        info = { "DiagnosticInfo", "#2563EB" },
-        hint = { "DiagnosticHint", "#10B981" },
-        default = { "Identifier", "#7C3AED" },
-        test = { "Identifier", "#FF00FF" },
-        link = { "#2563EB" },
-      },
-    },
   },
 }
 
